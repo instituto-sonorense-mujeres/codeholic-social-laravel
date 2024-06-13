cf8649f0e0c75a2bae082f067f258400882d952b
araso <araso@edesarrollos.info>
2024-06-12 22:57:04 -0700

    nueva estructura de vista perfil, timestamp part5 1:02:00

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -8,7 +8,7 @@
             class="ml-[48px] w-[128px] h-[128px] -mt-[64px]">
           <div class="flex justify-between items-center flex-1 p-4">
             <h2 class="font-bold text-lg">{{ user.name }}</h2>
-            <PrimaryButton v-if="isMyProfile">
+            <PrimaryButton>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-4 h-4 mr-2">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -21,47 +21,42 @@
       </div>
       <div class="border-t">
         <TabGroup>
-          <TabList class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-            <Tab v-for="category in Object.keys(categories)" as="template" :key="category" v-slot="{ selected }">
-              <button :class="[
-                'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
-                'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                selected
-                  ? 'bg-white text-blue-700 shadow'
-                  : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
-              ]">
-                {{ category }}
-              </button>
+          <TabList class="flex bg-white">
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Acerca De" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Posts" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Seguidores" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Siguiendo" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Fotos" :selected="selected" />
             </Tab>
           </TabList>
 
           <TabPanels class="mt-2">
-            <TabPanel v-for="(posts, idx) in Object.values(categories)" :key="idx" :class="[
-              'rounded-xl bg-white p-3',
-              'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-            ]">
-              <ul>
-                <li v-for="post in posts" :key="post.id" class="relative rounded-md p-3 hover:bg-gray-100">
-                  <h3 class="text-sm font-medium leading-5">
-                    {{ post.title }}
-                  </h3>
-
-                  <ul class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                    <li>{{ post.date }}</li>
-                    <li>&middot;</li>
-                    <li>{{ post.commentCount }} comments</li>
-                    <li>&middot;</li>
-                    <li>{{ post.shareCount }} shares</li>
-                  </ul>
-
-                  <a href="#" :class="[
-                    'absolute inset-0 rounded-md',
-                    'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2',
-                  ]" />
-                </li>
-              </ul>
+            <TabPanel>
+              <Edit :must-verify-email="mustVerifyEmail" :status="status" />
+            </TabPanel>
+            <TabPanel class="bg-white p-3 shadow">
+              Posts
+            </TabPanel>
+            <TabPanel class="bg-white p-3 shadow">
+              Seguidores
+            </TabPanel>
+            <TabPanel class="bg-white p-3 shadow">
+              Siguiendo
+            </TabPanel>
+            <TabPanel class="bg-white p-3 shadow">
+              Fotos
             </TabPanel>
           </TabPanels>
+
         </TabGroup>
       </div>
     </div>
@@ -69,64 +64,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 import { usePage } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-
+import TabItem from "@/Pages/Profile/Partials/TabItem.vue";
+import Edit from "@/Pages/Profile/Edit.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const user = usePage().props.auth.user;
 
-const categories = ref({
-  Recent: [
-    {
-      id: 1,
-      title: 'Does drinking coffee make you smarter?',
-      date: '5h ago',
-      commentCount: 5,
-      shareCount: 2,
+const props = defineProps({
+    mustVerifyEmail: {
+        type: Boolean,
     },
-    {
-      id: 2,
-      title: "So you've bought coffee... now what?",
-      date: '2h ago',
-      commentCount: 3,
-      shareCount: 2,
+    status: {
+        type: String,
     },
-  ],
-  Popular: [
-    {
-      id: 1,
-      title: 'Is tech making coffee better or worse?',
-      date: 'Jan 7',
-      commentCount: 29,
-      shareCount: 16,
-    },
-    {
-      id: 2,
-      title: 'The most innovative things happening in coffee',
-      date: 'Mar 19',
-      commentCount: 24,
-      shareCount: 12,
-    },
-  ],
-  Trending: [
-    {
-      id: 1,
-      title: 'Ask Me Anything: 10 answers to your questions about coffee',
-      date: '2d ago',
-      commentCount: 9,
-      shareCount: 5,
-    },
-    {
-      id: 2,
-      title: "The worst advice we've ever heard about coffee",
-      date: '4d ago',
-      commentCount: 1,
-      shareCount: 2,
-    },
-  ],
-})
+    user: {
+        type: Object
+    }
+});
 </script>
 
 
