afeb122f76f34870b88fc3cce81ee68c980f1208
araso <araso@edesarrollos.info>
2024-09-14 21:55:04 -0700

    PART-6 2:03:18 - trabajando en acciones para subir y modificar avatar

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -3,10 +3,8 @@
 
     <div class="max-w-[768px] mx-auto min-h-screen overflow-auto">
 
-      <div 
-        v-show="showNotification && status === 'cover-image-update'"
-        class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white"
-      >
+      <div v-show="showNotification && status === 'cover-image-update'"
+        class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white">
         Your cover image has been updated
       </div>
 
@@ -43,25 +41,30 @@
               Submit
             </button>
           </div>
-
-          <!-- <div class="flex gap-2 bg-white p-2 opacity-0 group-hover:opacity-100">
-            
-          </div> -->
         </div>
 
+
         <div class="flex">
-          <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp"
-            class="ml-[48px] w-[128px] h-[128px] -mt-[64px]">
-          <div class="flex justify-between items-center flex-1 p-4">
-            <h2 class="font-bold text-lg">{{ user.name }}</h2>
-            <PrimaryButton v-if="isMyProfile">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-4 h-4 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-              </svg>
-              Edit Profile
-            </PrimaryButton>
+          <div
+            class="flex items-center justify-center relative group/avatar -mt-[64px] ml-[48px] w-[128px] h-[128px] rounded-full">
+            <img :src="avatarImageSrc || user.avatar_url || '/img/default_avatar.webp'"
+              class="w-full h-full object-cover rounded-full">
+            <button v-if="!avatarImageSrc"
+              class="absolute left-0 top-0 right-0 bottom-0 bg-black/50 text-gray-200 rounded-full opacity-0 flex items-center justify-center group-hover/avatar:opacity-100">
+              <CameraIcon class="w-8 h-8" />
+
+              <input type="file" class="absolute left-0 top-0 bottom-0 right-0 opacity-0" @change="onAvatarChange" />
+            </button>
+            <div v-else class="absolute top-1 right-0 flex flex-col gap-2">
+              <button @click="resetAvatarImage"
+                class="w-7 h-7 flex items-center justify-center bg-red-500/80 text-white rounded-full">
+                <XMarkIcon class="h-5 w-5" />
+              </button>
+              <button @click="submitAvatarImage"
+                class="w-7 h-7 flex items-center justify-center bg-emerald-500/80 text-white rounded-full">
+                <CheckCircleIcon class="h-5 w-5" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -116,7 +119,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import TabItem from "@/Pages/Profile/Partials/TabItem.vue";
 import Edit from "@/Pages/Profile/Edit.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import { XMarkIcon, CheckCircleIcon } from '@heroicons/vue/24/solid';
+import { XMarkIcon, CheckCircleIcon, CameraIcon } from '@heroicons/vue/24/solid';
 import { useForm } from '@inertiajs/vue3'
 
 const imagesForm = useForm({
@@ -131,6 +134,7 @@ const authUser = usePage().props.auth.user;
 const isMyProfile = computed(() => authUser && authUser.id == props.user.id);
 
 const coverImageSrc = ref('');
+const avatarImageSrc = ref('')
 
 const props = defineProps({
   errors: Object,
@@ -157,9 +161,27 @@ const onCoverChange = (e) => {
   }
 }
 
+
+const onAvatarChange = (e) => {
+  imagesForm.avatar = e.target.files[0];
+  if (imagesForm.avatar) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log("load happens");
+      avatarImageSrc.value = reader.result;
+    }
+    reader.readAsDataURL(imagesForm.avatar);
+  }
+}
+
 const cancelCoverImage = () => {
   imagesForm.cover = null;
   coverImageSrc.value = null;
+}
+
+const cancelAvatarImage = () => {
+  imagesForm.avatar = null;
+  avatarImageSrc.value = null;
 }
 
 const submitCoverImage = () => {
@@ -168,6 +190,19 @@ const submitCoverImage = () => {
     onSuccess: (user) => {
       console.log({"usuario":user});
       cancelCoverImage();
+      setTimeout( () => {
+        showNotification.value = false;
+      })
+    }
+  });
+}
+
+const submitAvatarImage = () => {
+  console.log({"img":imagesForm.cover});
+  imagesForm.post(route('profile.updateCover'), {
+    onSuccess: (user) => {
+      console.log({"usuario":user});
+      cancelAvatarImage();
       setTimeout( () => {
         showNotification.value = false;
       })
