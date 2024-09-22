698044f5e89ffa29043ca388f2189ef1e4aff325
araso <araso@edesarrollos.info>
2024-09-21 17:56:43 -0700

    PART-7 0:01:00 - Profile information can be modified, only appears on tabpane 'My Profile', On track with commit 5dd12b1, behind commit cec9afb.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/u/{user:username}', [ProfileController::class, 'getProfile'])->name
 Route::middleware('auth')->group(function () {
   Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
     ->name('profile.updateImages');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
